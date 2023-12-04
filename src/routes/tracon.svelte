83a1d3c92b4ee334733c053c1221be575102b2d1
jessie846 <104696056+jessie846@users.noreply.github.com>
2023-12-04 07:39:37 -0500

    work on tracon ids

---
+++ src/routes/tracon.svelte
@@ -0,0 +1,16 @@
+<section>
+<button type="button">Emergency Information</button>
+<button type="button">SIA</button>
+<button type="button">Weather</button>
+<button type="button">VFR Sectional</button>
+<button type="button">Approach Charts</button>
+<button type="button">Publication</button>
+<button type="button">Main Menu</button>
+</section>
+
+<style>
+.button {
+    display: inline-block;
+}
+</style>
+```
